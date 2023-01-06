623fa6c251f36c852822a3763091069fa52af307
Nivin Anil <nivinanil244@gmail.com>
2023-01-06 16:30:36 +0530

    Profile and Firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBNSb3ENabycQEQybhVSrUNQhytJyU1qb8",
+    authDomain: "kodnest-dev.firebaseapp.com",
+    projectId: "kodnest-dev",
+    storageBucket: "kodnest-dev.appspot.com",
+    messagingSenderId: "368723037845",
+    appId: "1:368723037845:web:4bab8e2b02634d168d159c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export default app;
